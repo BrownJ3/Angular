91283cf28018380be78664a7159b7df32c4ea7ef
Josh Brown <brown.j3@gmial.com>
2017-05-29 19:51:55 -0400

    First commit of Angular 4 app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+ public items = [];
+ public newItem = "";
+ public completed = [];
+ public style1 = "";
+ public style2 = "line-through"
+
+ 
+
+
+
+ pushItem = function(){
+   if(this.newItem != ""){
+     this.items.push(this.newItem)
+     this.newItem = "";
+    }
+ }
+
+ removeItem = function(index){
+   this.items.splice(index, 1);
+ }
+
+ completeItem = function(index){
+  //  this.completed.push(this.index);
+   this.items.splice(index,0);
+  //  this.items.splice(index,1);
+   if(this.style1 == "" || this.style1 == null){
+      this.style = this.style2;
+  
+  }
+ }
 }
